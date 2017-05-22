fb9031eec430a38ced9445e331a459a5014e5833
Your Name <you@example.com>
2017-05-22 17:40:07 +0200

    init table User

--- app.js
+++ app.js
@@ -6,14 +6,14 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 
 var models = require('./models');
-models.sequelize.sync();
+// models.sequelize.sync();
+models.sequelize.sync({'force':'true'});
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'twig');
-
 
 app.use(express.static(path.join(__dirname, 'public')));
 
