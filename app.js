131bcf932d88b0748cc03efe354003f82aac9e58
Your Name <you@example.com>
2017-05-22 17:00:26 +0200

    remove all jad, route, models and add distante BDD

--- app.js
+++ app.js
@@ -1,15 +1,9 @@
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-var students = require('./routes/students');
-var school = require('./routes/school');
-var project = require('./routes/project');
 
 var models = require('./models');
 models.sequelize.sync();
@@ -20,19 +14,11 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'twig');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-app.use('/students', students);
-app.use('/school', school);
-app.use('/project', project);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
