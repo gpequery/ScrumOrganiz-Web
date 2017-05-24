6196ee9f0cdd47e5efc1888c64e46087f3f55451
Your Name <you@example.com>
2017-05-24 18:21:08 +0200

    HomePage redirection login, signup FOrmulaire de redirection en post

--- public/javascripts/scripts/_template.js
+++ public/javascripts/scripts/_template.js
@@ -1,3 +1,8 @@
 $j(function () {
 
+    /* Enables POST navigation everywhere */
+    $j('.navigate').on('click', function() {
+        $j('.formNavigate').attr('action', $j(this).attr('data-url'));
+        $j('.formNavigate').submit();
+    })
 });
