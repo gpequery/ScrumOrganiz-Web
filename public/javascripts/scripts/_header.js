1ef9c94f67b71fd7efd2cf4619e32b9433502324
gpequery <you@example.com>
2017-06-18 18:08:40 +0200

    init new project page, css & js

--- public/javascripts/scripts/_header.js
+++ public/javascripts/scripts/_header.js
@@ -1,9 +1,10 @@
 $j(function () {
-    /* Enables POST navigation everywhere */
-    $j('.headOption.pseudo').on('click', function() {
+    /* open popin */
+    $j('.headOption.setting').on('click', function() {
         $j('.popinHeaderRight.pseudo').css('display') == 'none' ? $j('.popinHeaderRight.pseudo').show('slow') : $j('.popinHeaderRight.pseudo').hide('slow');
     });
 
+    /* close session before exit */
     $j('.popinHeaderRight .logout').on('click', function() {
         $j.post(
             '/user/closeSession', {
